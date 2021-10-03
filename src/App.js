f7681ad678d8e7896884f9e81f7a682b4728676b
ivansinabdeev <ivansinabdeev@gmail.com>
2021-10-03 20:59:40 +0200

    end task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Form from './components/Form/Form';
+import Title from './components/Title/Title';
+import Filter from './components/Filter/Filter';
+import ContactList from './components/ContactList/ContactList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  handleSubmit = contact => {
+    this.state.contacts.forEach(el => {
+      if (el.name.toLowerCase() === contact.name.toLowerCase()) {
+        contact.name = 'repeat';
+        return alert('contact already exist at phonebook');
+      }
+    });
+    if (contact.name === 'repeat') {
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+
+  handleRemove = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getvisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) console.log('работает!');
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const visibleContacts = this.getvisibleContacts();
+
+    return (
+      <div className="App">
+        <Form onSubmit={this.handleSubmit} />
+        {contacts.length ? <Title title="Contacts" /> : null}
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ul>
+          {visibleContacts.map(contact => (
+            <ContactList
+              key={contact.id}
+              id={contact.id}
+              name={contact.name}
+              number={contact.number}
+              handleClick={this.handleRemove}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
